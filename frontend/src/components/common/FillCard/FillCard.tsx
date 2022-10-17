8702c7399817bb5da8eebb1812715801a5ea5040
YushiroDodo63 <yushiro0603@gmail.com>
2022-10-17 11:27:40 +0900

    dashboardページの作成

--- frontend/src/components/common/FillCard/FillCard.tsx
+++ frontend/src/components/common/FillCard/FillCard.tsx
@@ -5,15 +5,18 @@ import s from './FillCard.module.css'
 
 interface Props {
   children?: React.ReactNode
+  width?: string
+  height?: string
 }
 
 function Card(props: Props): JSX.Element {
   return (
-    <div className={clsx('mx-auto w-4/5')}>
+    <div className={clsx('mx-auto', props.width ? props.width : 'w-4/5')}>
       <div
         className={clsx(
-          'border-primary-2 border-opacity-1 shadow-primary-2 hover:border-accent-2 m-10 rounded-lg border px-10 py-5 shadow-md hover:border',
+          'border-primary-2 border-opacity-1 shadow-primary-2 hover:border-accent-2 m-10 rounded-lg border shadow-md hover:border',
           s.card,
+          props.height ? props.height : 'h-auto',
         )}
       >
         {props.children}
