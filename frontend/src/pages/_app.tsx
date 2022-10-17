9553dd3da4afaeaf6d8ad4679e101b4dfa988315
YushiroDodo63 <yushiro0603@gmail.com>
2022-10-17 17:55:15 +0900

    add header and layout(#11)(#12)

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,11 +1,14 @@
 import '../styles/globals.css'
 import { RecoilRoot } from 'recoil'
 import type { AppProps } from 'next/app'
+import MainLayout from '@components/Layout/MainLayout'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <RecoilRoot>
-      <Component {...pageProps} />
+      <MainLayout>
+        <Component {...pageProps} />
+      </MainLayout>
     </RecoilRoot>
   )
 }
