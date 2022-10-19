d52256896554b65130674a46bce90b4fd4679066
YushiroDodo63 <yushiro0603@gmail.com>
2022-10-19 21:33:12 +0900

    add close icon

--- frontend/src/components/icons/index.ts
+++ frontend/src/components/icons/index.ts
@@ -1,3 +1,4 @@
 export { default as Account } from './Account'
+export { default as Close } from './Close'
 export { default as Logo } from './Logo'
 export { default as WhiteLogo } from './WhiteLogo'
