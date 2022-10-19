dd03bb4803ffab806eb33f38f40efcfeaa35d941
YushiroDodo63 <yushiro0603@gmail.com>
2022-10-19 21:34:06 +0900

    add offer Modal(#31)

--- frontend/src/pages/rentlist/index.tsx
+++ frontend/src/pages/rentlist/index.tsx
@@ -1,23 +1,48 @@
+import { useState, useEffect, memo, useRef } from 'react'
 import type { NextPage } from 'next'
-import { Card, Label } from '@components/common'
 import clsx from 'clsx'
-import { User, Area, Value, Tag } from '@type/common'
-import { useEffect, memo, useRef } from 'react'
+import { Card, Label, Modal, PrimaryButton, Textarea } from '@components/common'
+import { User, Area, Value, Tag, PersonalInfo, CompanyInfo } from '@type/common'
+import { Close } from '@components/icons'
 
 interface Props {
-  tooltipText: string
   children?: React.ReactNode
   data: any
 }
 
 const Rentlist: NextPage = () => {
-  const user: User = {
+  const personalInfo: PersonalInfo = {
+    id: 1,
+    familyName: '藤崎',
+    firstName: '竜成',
+    familyNameKana: 'フジサキ',
+    firstNameKana: 'リュウセイ',
+    birthDay: '19990202',
+    phoneNumber: '09012345678',
+    userId: 1,
+    created_at: '',
+    updated_at: '',
+  }
+  const companyInfo: CompanyInfo = {
+    id: 1,
+    name: '長岡金型',
+    phoneNumber: '09012345678',
+    postCode: '9456666',
+    prefecture: '新潟県',
+    city: '長岡市西陵町',
+    addressNumber: '2674-31',
+    buildingName: '長岡金型ビル',
+    website: 'https://nagaoka-kanagata.co.jp',
+    created_at: '',
+    updated_at: '',
+  }
+  const user = {
     id: 1,
     name: 'test',
     email: 'test',
     password: 'test',
-    personalInfoId: 1,
-    companyInfoId: 1,
+    personalInfo: personalInfo,
+    companyInfo: null,
     created_at: '',
     updated_at: '',
   }
@@ -130,6 +155,7 @@ const Rentlist: NextPage = () => {
     // 表示する条件を切り替えるためのref
     const defaultContentRef = useRef<HTMLDivElement>(null)
     const subContentRef = useRef<HTMLDivElement>(null)
+    const modalRef = useRef<HTMLDivElement>(null)
 
     useEffect(() => {
       // if文を使って、inputRefのcurrentプロパティが存在するか確認
@@ -146,7 +172,8 @@ const Rentlist: NextPage = () => {
 
     // マウスが乗ったら「その他の条件」を表示
     const handleMouseEnter = () => {
-      if (!defaultContentRef.current || !subContentRef.current) return
+      if (!defaultContentRef.current && modalRef.current) return
+      else if (!defaultContentRef.current || !subContentRef.current) return
       defaultContentRef.current.style.opacity = '0'
       defaultContentRef.current.style.visibility = 'hidden'
       defaultContentRef.current.style.height = '0px'
@@ -168,6 +195,10 @@ const Rentlist: NextPage = () => {
       subContentRef.current.style.height = '0px'
       subContentRef.current.style.width = '0px'
     }
+
+    const [isOpen, setIsOpen] = useState<boolean>(false)
+    const onOpen = () => setIsOpen(true)
+    const onClose = () => setIsOpen(false)
 
     // 希望する条件を表示
     const defaultContent = (
@@ -214,16 +245,11 @@ const Rentlist: NextPage = () => {
             ),
           )}
         </div>
-        {/* <div className="py-2">
-                <span className="border-primary-1 border-b-2 text-left text-xl font-bold">
-                  より詳しい条件
-                </span>
-              </div> */}
         <div className="text-left">
           {props.data.request.detail.classification.tag.map((taglist: any) => (
             <>
               {taglist.name === '間取り' && (
-                <div className="py-1 text-lg font-bold">
+                <div className="py-2 text-lg font-bold">
                   {taglist.name}
                   <br />
                   <div className="w-1/1 flex flex-wrap text-sm">
@@ -231,7 +257,7 @@ const Rentlist: NextPage = () => {
                       <Label
                         name={tag.name}
                         width={'w-auto'}
-                        className={'m-1'}
+                        className={'mx-1 mt-2 '}
                       />
                     ))}
                   </div>
@@ -295,7 +321,7 @@ const Rentlist: NextPage = () => {
           {data.request.detail.classification.tag.map((taglist) => (
             <>
               {taglist.name != '間取り' && (
-                <div className="py-1 text-lg font-bold">
+                <div className="pb-2 text-lg font-bold">
                   {taglist.name}
                   <br />
 
@@ -304,7 +330,7 @@ const Rentlist: NextPage = () => {
                       <Label
                         name={tag.name}
                         width={'w-auto'}
-                        className={'m-1'}
+                        className={'mx-1 mt-2 '}
                       />
                     ))}
                   </div>
@@ -312,6 +338,9 @@ const Rentlist: NextPage = () => {
               )}
             </>
           ))}
+          <div className="border-primary-1 mt-2 flex justify-center border-t-2 border-dotted pt-4 pb-2 text-xl font-bold">
+            <PrimaryButton onClick={onOpen}>オファーを送る</PrimaryButton>
+          </div>
         </div>
       </>
     )
@@ -333,6 +362,88 @@ const Rentlist: NextPage = () => {
         >
           {subContent}
         </div>
+        <div ref={modalRef}>
+          {isOpen && (
+            <Modal>
+              <div
+                className={clsx(
+                  'justify-left ml-4 mb-4 w-fit cursor-pointer items-center',
+                )}
+              >
+                <Close width="32" height="32" onClick={onClose} />
+              </div>
+              <div className="border-primary-1 mb-4 border-l-8 pl-2 text-left text-3xl font-bold">
+                オファーを送る
+              </div>
+              <div>
+                <span className="border-primary-1 border-b-2 text-left text-xl font-bold">
+                  希望する条件
+                </span>
+              </div>
+              <div className="grid grid-cols-4 text-left">
+                {props.data.request.detail.classification.range.map(
+                  (range: any) => (
+                    <span className="pt-2 pb-1 text-lg font-bold">
+                      {range.name}
+                      <br />
+                      <span className="text-sm">
+                        {range.values.map((value: Value, index: number) => (
+                          <span>
+                            {value.name}
+                            {index < range.values.length - 1 && '〜'}
+                          </span>
+                        ))}
+                      </span>
+                    </span>
+                  ),
+                )}
+                {props.data.request.detail.classification.value.map(
+                  (valuelist: any) => (
+                    <>
+                      {valuelist.name === '築年数' && (
+                        <span className="pt-2 pb-1 text-lg font-bold">
+                          {valuelist.name}
+                          <br />
+                          <span className="text-sm">
+                            {valuelist.values.map((value: Value) => (
+                              <span>{value.name}</span>
+                            ))}
+                          </span>
+                        </span>
+                      )}
+                    </>
+                  ),
+                )}
+              </div>
+              <div className="text-left">
+                {props.data.request.detail.classification.tag.map(
+                  (taglist: any) => (
+                    <div className="py-1 text-lg font-bold">
+                      {taglist.name}
+                      <br />
+                      <div className="w-1/1 flex flex-wrap text-sm">
+                        {taglist.tags.map((tag: Tag) => (
+                          <Label
+                            name={tag.name}
+                            width={'w-auto'}
+                            className={'m-1'}
+                          />
+                        ))}
+                      </div>
+                    </div>
+                  ),
+                )}
+              </div>
+              <div className="border-primary-1 mb-2 border-b-2 text-left text-xl font-bold">
+                オファー内容
+              </div>
+              <Textarea className=" mt-3" />
+              <div className="border-primary-1 mt-2 flex justify-center pt-4 pb-2 text-2xl font-bold">
+                <PrimaryButton onClick={onOpen}>オファーを送る</PrimaryButton>
+              </div>
+            </Modal>
+          )}
+        </div>
       </div>
     )
   })
@@ -348,9 +459,7 @@ const Rentlist: NextPage = () => {
       <div className="grid w-full sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-4">
         {dataList.map((data) => (
           <Card width="w-1/1">
-            <CardContent tooltipText="ツールチップの文言だよ" data={data}>
-              アイコンとか
-            </CardContent>
+            <CardContent data={data}></CardContent>
           </Card>
         ))}
       </div>
