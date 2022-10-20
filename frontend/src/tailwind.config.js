e0a41296dc77b32a82c591d3e41b1c7e79227a21
YushiroDodo63 <yushiro0603@gmail.com>
2022-10-20 19:13:14 +0900

    set darkmode class

--- frontend/src/tailwind.config.js
+++ frontend/src/tailwind.config.js
@@ -2,7 +2,7 @@
 const colors = require('tailwindcss/colors')
 
 module.exports = {
-  darkMode: 'false',
+  darkMode: 'class',
   content: [
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
