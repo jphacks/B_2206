a1375a3e0cbf82525ca2a2fd6fe484f14f490d0b
imaimai17468 <tosiki_801_life@outlook.jp>
2022-10-21 03:54:47 +0900

    add progress bar

--- frontend/src/pages/conditions/city/index.tsx
+++ frontend/src/pages/conditions/city/index.tsx
@@ -268,6 +268,39 @@ function City(props: Props): JSX.Element {
 
       <main className={styles.main}>
         <div className="w-full">
+          <div className="flex justify-center">
+            <div className="w-2/3">
+              <h2 className="sr-only">Steps</h2>
+              <div className="after:bg-primary-1 relative after:absolute after:inset-x-0 after:top-1/2 after:block after:h-0.5 after:-translate-y-1/2 after:rounded-lg">
+                <ol className="text-main-black relative z-10 flex justify-between text-sm font-medium">
+                  <li className="bg-background-1 flex items-center p-2">
+                    <span className="bg-primary-1 h-6 w-6 rounded-full text-center text-[10px] font-bold leading-6">
+                      1
+                    </span>
+                    <span className="hidden sm:ml-2 sm:block"> 県の選択 </span>
+                  </li>
+                  <li className="bg-background-1 flex items-center p-2">
+                    <span className="bg-accent-2 h-6 w-6 rounded-full text-center text-[10px] font-bold leading-6 text-white">
+                      2
+                    </span>
+                    <span className="hidden sm:ml-2 sm:block">
+                      {' '}
+                      市区町村の選択{' '}
+                    </span>
+                  </li>
+                  <li className="bg-background-1 flex items-center p-2">
+                    <span className="bg-primary-1 h-6 w-6 rounded-full text-center text-[10px] font-bold leading-6">
+                      3
+                    </span>
+                    <span className="hidden sm:ml-2 sm:block">
+                      {' '}
+                      お部屋条件の選択{' '}
+                    </span>
+                  </li>
+                </ol>
+              </div>
+            </div>
+          </div>
           <Card width={'w-4/5'}>
             <div className={'flex justify-start pb-8'}>
               <PrimaryButton
@@ -282,7 +315,7 @@ function City(props: Props): JSX.Element {
               <p>{user.prefectureName}-市区町村を選択</p>
             </div>
             <div>
-              <p>市区町村にチェックを入れてください</p>
+              <p>市区町村にチェックを入れてくださ</p>
               <div
                 className={'border-primary-1 mb-5 border-2 border-dashed'}
               ></div>
