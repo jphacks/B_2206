35b35959245efbaca5499109d3e9de3fd8a9fc41
YushiroDodo63 <yushiro0603@gmail.com>
2022-10-22 11:18:48 +0900

    mod toppage sp view

--- frontend/src/components/common/FillCard/FillCard.tsx
+++ frontend/src/components/common/FillCard/FillCard.tsx
@@ -7,16 +7,19 @@ interface Props {
   children?: React.ReactNode
   width?: string
   height?: string
+  m?: string
 }
 
 function Card(props: Props): JSX.Element {
+  const m = props.m ? props.m : ' m-10 '
   return (
     <div className={clsx('mx-auto', props.width ? props.width : 'w-4/5')}>
       <div
         className={clsx(
-          'border-primary-2 border-opacity-1 shadow-primary-2 hover:border-accent-2 m-10 rounded-lg border shadow-md hover:border',
+          'border-primary-2 border-opacity-1 shadow-primary-2 hover:border-accent-2 rounded-lg border shadow-md hover:border',
           s.card,
           props.height ? props.height : 'h-auto',
+          m,
         )}
       >
         {props.children}
